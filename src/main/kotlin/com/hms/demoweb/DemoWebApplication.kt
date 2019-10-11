17ca154a5b3af7b65820edfd1eec799eb86f9ea8
sazzad <sazzad@hsenidmobile.com>
2019-10-11 13:34:29 +0600

    init

---
+++ src/main/kotlin/com/hms/demoweb/DemoWebApplication.kt
@@ -0,0 +1,22 @@
+package com.hms.demoweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoWebApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/demo")
+class DemoController{
+
+	@GetMapping
+	fun demoGet() = "demo"
+}
